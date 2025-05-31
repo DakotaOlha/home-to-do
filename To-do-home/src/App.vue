8c5f18a3cdca63fa965635172aa74ab54fa739ed
Dakota <ochaiyk2006@gmail.com>
2025-05-31 19:07:41 +0300

    change vue

---
+++ To-do-home/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import HomePage from './views/HomePage.vue'
+import AppLayout from './layouts/AppLayout.vue'
+
+const router = useRouter()
+const isAuthenticated = ref(false)
+
+const handleLogin = () => {
+  isAuthenticated.value = true
+  router.push('/tasks')
+}
+
+const handleLogout = () => {
+  isAuthenticated.value = false
+  router.push('/')
+}
+</script>
+
+<template>
+  <RouterView v-if="!isAuthenticated" />
+  <AppLayout v-else @logout="handleLogout">
+    <RouterView />
+  </AppLayout>
+</template>
